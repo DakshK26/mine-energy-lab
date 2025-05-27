e55c7935737187d131de110b043802ee3706b5cf
DakshK26 <khannad24@gmail.com>
2025-05-27 18:40:26 -0400

    Add Monthly Data

--- case_study/file_loader.py
+++ case_study/file_loader.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import numpy as np
+import click as ui
 
-def speedsAsArray():
-    df = pd.read_csv("case_study/jan.csv")
+def speedsAsArray(month: str):
+    df = pd.read_csv(f"case_study/months_data/{month}.csv")
     speeds_kmh = df['Wind Spd (km/h)'].dropna().to_numpy() # Drop all empty entries and get wind speeds
     speeds_ms = speeds_kmh / 3.6
     return speeds_ms
 
 
 if __name__ == "__main__":
-    speeds_ms = speedsAsArray()
+    month = ui.prompt("Month: ")
+    speeds_ms = speedsAsArray(month)
     # Find mean and std
     print(f"Mean wind speed: {np.mean(speeds_ms)}")
     print(f"Standard Deviation: {np.std(speeds_ms)}")
